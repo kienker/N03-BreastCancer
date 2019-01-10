07163f85f194b94b85bcd664be0cfd7cd9f43959
kienker <46550517+kienker@users.noreply.github.com>
2019-01-10 21:18:35 +0700

    Gửi thầy
    Bài thi giữa kỳ

---
+++ Bai 2 MSSV 1851063187.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<math.h>
+using namespace std ;
+
+int manin()
+{
+	double tienkivong;
+	double nam;
+	double tienvon;
+	double laisuat;
+	double a,b;
+	cout<<"nhap so tien ki vong: ";
+	cin>>tienkivong;
+	cout<< "nhap so tien von:";
+	cin>>tienvon;
+	cout<<"so lai suat";
+	cin>>laisuat;
+	if(tienkivong>tienvon)
+	{
+	laisuat=double(laisuat/100);
+	a=double(tienkivong/tienvon);
+	b=double(1+laisuat);
+	nam=double(log(a)/log(b));
+	cout<<"so nam ki vongla:"<<nam;
+	}
+	else
+	cout<<"khong hop le";
+	return 0;
+	
+}
